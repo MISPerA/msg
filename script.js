3ae3362a211bb940eecf2061f9f529a778198fc1
MISPerA <143564926+MISPerA@users.noreply.github.com>
2023-12-06 12:44:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     // URL da sua planilha
-    var spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1tOocVgAMV_M28jMX-_gQ0pWUwFOZUaV_RdhBJbjDKqk/edit#gid=1611553868';
+    var spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1tOocVgAMV_M28jMX-_gQ0pWUwFOZUaV_RdhBJbjDKqk/edit';
 
     // Obtém as mensagens da planilha
     function getMessages() {
